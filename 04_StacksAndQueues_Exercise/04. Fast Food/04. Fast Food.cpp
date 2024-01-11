236772862f7fe4e20bbb576c249315e456e9a850
MZhikoV <m.zhikov@gmail.com>
2024-01-11 15:24:03 +0200

    Task 04 solution

---
+++ 04_StacksAndQueues_Exercise/04. Fast Food/04. Fast Food.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <sstream>
+#include <climits>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    string input;
+    getline(cin, input);
+
+    istringstream inp1(input);
+    int food;
+    inp1 >> food;
+
+    getline(cin, input);
+
+    istringstream inp(input);
+
+    queue<int>orders;
+
+    int max = INT_MIN;
+
+    int order;
+    while (inp >> order) {
+        orders.push(order);
+        if (order > max) {
+            max = order;
+        }
+    }
+
+    cout << max << endl;
+    bool notenough = false;
+
+    while (!orders.empty()) {
+        food -= orders.front();
+        if (food <= 0 && !notenough) {
+            cout << "Orders left: ";
+            notenough = true;
+        }
+        if (notenough) {
+            cout << orders.front() << ' ';
+        }
+        orders.pop();
+    }
+    if (food > 0) {
+        cout << "Orders complete" << endl;
+    }
+return 0;
+}
