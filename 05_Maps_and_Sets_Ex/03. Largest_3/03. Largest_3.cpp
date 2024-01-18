1fedb111284e00b03eb0040f699eed8889146fba
MZhikoV <m.zhikov@gmail.com>
2024-01-18 16:23:27 +0200

    modified

--- 05_Maps_and_Sets_Ex/03. Largest_3/03. Largest_3.cpp
+++ 05_Maps_and_Sets_Ex/03. Largest_3/03. Largest_3.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <functional>
+#include <cctype>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	vector<int> numbers;
+
+	string input;
+	getline(cin, input);
+
+	istringstream inp(input);
+
+	int number;
+	while (inp >> number) {
+		numbers.push_back(number);
+	}
+
+	sort(numbers.begin(), numbers.end(), greater<int>());
+
+	int counter = 0;
+	for (int num : numbers) {
+		if (counter == 3) {
+			break;
+		}
+		cout << num << ' ';
+		counter++;
+	}
+	cout << endl;
+	return 0;
 }
+
+
+//Using set for non-repeatable numbers
+/*
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <set>
+#include <cctype>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+int main()
+{
+	set<int, greater<int>> numbers;
+
+	string input;
+	getline(cin, input);
+
+	istringstream inp(input);
+
+	int number;
+	while (inp >> number) {
+		numbers.insert(number);
+	}
+	int counter = 0;
+	for (int num : numbers) {
+		if (counter == 3) {
+			break;
+		}
+		cout << num << ' ';
+		counter++;
+	}
+	cout << endl;
+	return 0;
+}
+*/
