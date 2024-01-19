f486a462781468a5ca58eeba07688b899d69dd09
MZhikoV <m.zhikov@gmail.com>
2024-01-19 17:33:00 +0200

    Task 04 solution

--- 07_Multidimensional_Arrays_Lab/03. Symbol in Matrix/03. Symbol in Matrix.cpp
+++ 07_Multidimensional_Arrays_Lab/03. Symbol in Matrix/03. Symbol in Matrix.cpp
@@ -1,20 +1,38 @@
-// 03. Symbol in Matrix.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+const int MAX = 1000;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int row;
+    cin >> row;
+    char arr[MAX][MAX];
+
+    for (size_t i = 0; i < row; i++) {
+        for (size_t j = 0; j < row; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    char symb;
+    cin >> symb;
+
+    bool isThere = false;
+
+
+    for (size_t i = 0; i < row; i++) {
+        for (size_t j = 0; j < row; j++) {
+            if (arr[i][j] == symb) {
+                isThere = true;
+                cout << '(' << i << ", " << j << ')' << endl;
+                exit(0);
+            }
+        }
+    }
+    if (!isThere) {
+        cout << symb << " does not occur in the matrix" << endl;
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- 07_Multidimensional_Arrays_Lab/04. Diagonal Difference/04. Diagonal Difference.cpp
+++ 07_Multidimensional_Arrays_Lab/04. Diagonal Difference/04. Diagonal Difference.cpp
@@ -8,17 +8,26 @@ int main()
     int row;
     cin >> row;
     int arr[MAX][MAX];
-    
-    int sum = 0;
+
+    int sumP = 0;
 
     for (size_t i = 0; i < row; i++) {
         for (size_t j = 0; j < row; j++) {
             cin >> arr[i][j];
             if (i == j) {
-                sum += arr[i][j];
+                sumP += arr[i][j];
             }
         }
     }
+
+    int sumS = 0;
+
+            for (size_t j = 0; j < row; j++) {
+             sumS += arr[row-1-j][j];
+            }
+
+int sum = abs(sumP + sumS);
+
     cout << sum << endl;
     return 0;
 }
