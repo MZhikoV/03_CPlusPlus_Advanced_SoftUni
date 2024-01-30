f889e7f78c47ff3597b25c60ec81370533a8a1f9
MZhikoV <m.zhikov@gmail.com>
2024-01-30 19:45:25 +0200

    Task 07 solution

---
+++ 99_Test_Practice/Map_and_Set_Lab/07. Sets of Elements.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void readSet(set<int> & inp,int num){
+while (num--){
+    int data;
+    cin>>data;
+
+    inp.insert(data);
+}
+}
+
+int main() {
+
+    set<int>first;
+    set<int>second;
+
+    int n,m;
+    cin>>n>>m;
+
+    readSet(first,n);
+    readSet(second,m);
+
+    for (int num:first) {
+        auto it=second.find(num);
+
+        if(it!=second.end()){
+            cout<<num<<' ';
+        }
+    }
+    cout<<endl;
+    return 0;
+}
