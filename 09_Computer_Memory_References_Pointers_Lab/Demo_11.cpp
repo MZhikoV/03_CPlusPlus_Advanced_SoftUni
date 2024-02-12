5ac89d82f920b3b41e127b77b817bb9238f57a8e
MZhikoV <m.zhikov@gmail.com>
2024-02-12 18:30:49 +0200

    Demo 11

---
+++ 09_Computer_Memory_References_Pointers_Lab/Demo_11.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int arr[3] {13,42,69};
+    
+    cout<<0[arr]<<endl;
+   
+    cout<<1[arr]<<endl;
+
+    cout<<2[arr]<<endl;
+    
+    system("pause");
+    return 0;
+}
