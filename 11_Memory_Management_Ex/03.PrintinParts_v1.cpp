60bc2c3ce51237ea51fdabfec1b75eaf9668e562
MZhikoV <m.zhikov@gmail.com>
2024-02-18 16:05:15 +0200

    Task 03 solution v1 by TT

---
+++ 11_Memory_Management_Ex/03.PrintinParts_v1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+int m,n;
+cin>>n>>m;
+
+int totalPos=n*m;
+
+int * flatMtrix= new int[totalPos];
+
+for (int i=0;i<totalPos;i++) {
+    cin>>flatMtrix[i];
+}
+
+int r,c;
+cin>>r>>c;
+
+for (int i=0;i<r;i++) {
+    for (int j=0;j<c;j++){
+        cout<<flatMtrix[i*m+j]<<' ';
+    }
+    cout<<endl;
+}
+    system("pause");
+    return 0;
+}
